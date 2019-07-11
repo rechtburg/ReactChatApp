f995a08c50ab7486c6ea4c181f77bb83262e7b85
rechtburg <y.shirota.research.a.develop@gmail.com>
2019-07-12 05:25:59 +0900

    modify webpack entry

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    bundle: './src/app.tsx',
+    main: './src/app.tsx',
   },
   output: {
     path: path.resolve(__dirname, 'public'),
